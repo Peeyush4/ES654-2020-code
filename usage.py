b8d78fe245d33552f772f8295ea937420b484057
Peeyush4 <32363748+Peeyush4@users.noreply.github.com>
2020-01-17 00:40:41 +0530

    Added max_depth for each Test case

--- usage.py
+++ usage.py
@@ -24,7 +24,7 @@ y = pd.Series(np.random.randn(N))
 
 
 for criteria in ['information_gain', 'gini_index']:
-    tree = DecisionTree(criterion=criteria) #Split based on Inf. Gain
+    tree = DecisionTree(criterion=criteria, max_depth=3) #Split based on Inf. Gain
     tree.fit(X, y)
     y_hat = tree.predict(X)
     tree.plot()
@@ -38,10 +38,10 @@ for criteria in ['information_gain', 'gini_index']:
 N = 30
 P = 5
 X = pd.DataFrame(np.random.randn(N, P))
-y = pd.Series(np.random.randint(P, size = N), dtype="category")
+y = pd.Series(np.random.randint(P, size = N, ), dtype="category")
 
 for criteria in ['information_gain', 'gini_index']:
-    tree = DecisionTree(criterion=criteria) #Split based on Inf. Gain
+    tree = DecisionTree(criterion=criteria, max_depth=3) #Split based on Inf. Gain
     tree.fit(X, y)
     y_hat = tree.predict(X)
     tree.plot()
@@ -58,10 +58,10 @@ for criteria in ['information_gain', 'gini_index']:
 N = 30
 P = 5
 X = pd.DataFrame({i:pd.Series(np.random.randint(P, size = N), dtype="category") for i in range(5)})
-y = pd.Series(np.random.randint(P, size = N), , dtype="category")
+y = pd.Series(np.random.randint(P, size = N), dtype="category")
 
 for criteria in ['information_gain', 'gini_index']:
-    tree = DecisionTree(criterion=criteria) #Split based on Inf. Gain
+    tree = DecisionTree(criterion=criteria, max_depth=3) #Split based on Inf. Gain
     tree.fit(X, y)
     y_hat = tree.predict(X)
     tree.plot()
@@ -80,7 +80,7 @@ X = pd.DataFrame({i:pd.Series(np.random.randint(P, size = N), dtype="category") 
 y = pd.Series(np.random.randn(N))
 
 for criteria in ['information_gain', 'gini_index']:
-    tree = DecisionTree(criterion=criteria) #Split based on Inf. Gain
+    tree = DecisionTree(criterion=criteria, max_depth=3) #Split based on Inf. Gain
     tree.fit(X, y)
     y_hat = tree.predict(X)
     tree.plot()
